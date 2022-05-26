22c22d9595ddc54755be2fecda43b931a2d8d1de
Nat34 <nathan.a.thomas@gmail.com>
2022-05-25 20:20:01 -0500

    add UIAlertController action, UIToolbar, UIProgressView

--- Project4/Project4/ViewController.swift
+++ Project4/Project4/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate{
+class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
     
     override func loadView() {
@@ -22,9 +22,16 @@ class ViewController: UIViewController, WKNavigationDelegate{
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+
+        toolbarItems = [spacer, refresh]
+        navigationController?.isToolbarHidden = false
+        
         let url = URL(string: "https://www.hackingwithswift.com")!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        
         
     }
     
@@ -38,8 +45,13 @@ class ViewController: UIViewController, WKNavigationDelegate{
     }
     
     func openPage(action: UIAlertAction) {
-        let url = URL(string: "https://" + action.title!)!
+        guard let actionTitle = action.title else {return}
+        guard let url = URL(string: "https://" + actionTitle) else {return}
         webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 }
 
