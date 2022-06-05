9b15916cd653678d4e036080a1592a53089ed078
Nat34 <nathan.a.thomas@gmail.com>
2022-06-05 17:08:36 -0500

    add protocol to handle signature of delegate, implementmethod in controller

---
+++ Design Patterns/ViewDelegates/ViewDelegates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewDelegates
+//
+//  Created by Nathan Thomas on 6/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
