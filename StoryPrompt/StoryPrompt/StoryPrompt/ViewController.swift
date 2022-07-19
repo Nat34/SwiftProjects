e79a2e25c79f5c9821557862953e47a7bf3d355f
Nat34 <nathan.a.thomas@gmail.com>
2022-07-18 20:56:31 -0500

    init commit

---
+++ StoryPrompt/StoryPrompt/StoryPrompt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryPrompt
+//
+//  Created by Nathan Thomas on 7/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
